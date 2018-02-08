02b088afab75553a8c4669d8594f68fd54166802
Adrienne <amarshall813@gmail.com>
2018-02-08 15:39:57 -0800

    removed .R from main folder

--- assignment4.R
+++
@@ -1,7 +0,0 @@
-readers <- c("Adrienne", "Becky", "Courtney", "Danny", 
-           "Meghan", "Micah", "Paris", "Shana")
-readers <- rep(readers, 203)
-readers <- sample(readers)
-
-round4 <- write.csv(readers, "round4.csv", row.names=FALSE)
-
